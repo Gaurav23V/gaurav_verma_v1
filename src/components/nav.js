236e380fa21c91f51857d4dbbad216e0b1babe2b
Gaurav23V <gaurav2301v@gmail.com>
2024-08-29 22:02:41 +0530

    fixed navbar

--- src/components/nav.js
+++ src/components/nav.js
@@ -32,26 +32,24 @@ const StyledHeader = styled.header`
     padding: 0 25px;
   }
 
-  @media (prefers-reduced-motion: no-preference) {
-    ${(props) =>
-      props.scrollDirection === "up" &&
-      !props.scrolledToTop &&
-      css`
-        height: var(--nav-scroll-height);
-        transform: translateY(0px);
-        background-color: rgba(10, 25, 47, 0.85);
-        box-shadow: 0 10px 30px -10px var(--navy-shadow);
-      `};
+  ${(props) =>
+    props.$scrollDirection === "up" &&
+    !props.$scrolledToTop &&
+    css`
+      height: var(--nav-scroll-height);
+      transform: translateY(0px);
+      background-color: rgba(10, 25, 47, 0.85);
+      box-shadow: 0 10px 30px -10px var(--navy-shadow);
+    `};
 
-    ${(props) =>
-      props.scrollDirection === "down" &&
-      !props.scrolledToTop &&
-      css`
-        height: var(--nav-scroll-height);
-        transform: translateY(calc(var(--nav-scroll-height) * -1));
-        box-shadow: 0 10px 30px -10px var(--navy-shadow);
-      `};
-  }
+  ${(props) =>
+    props.$scrollDirection === "down" &&
+    !props.$scrolledToTop &&
+    css`
+      height: var(--nav-scroll-height);
+      transform: translateY(calc(var(--nav-scroll-height) * -1));
+      box-shadow: 0 10px 30px -10px var(--navy-shadow);
+    `};
 `;
 
 const StyledNav = styled.nav`
